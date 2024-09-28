77861bd7fcfd799b9508b4fd251b8d6415d62fee
Trevor Manz <trevor.j.manz@gmail.com>
2024-09-28 17:26:59 -0400

    Add csv

--- main.ts
+++ main.ts
@@ -3,16 +3,16 @@ import he from "npm:he@1.2.0";
 import ProgressBar from "jsr:@deno-library/progress@1.4.9";
 import { assert } from "jsr:@std/assert@1.0.6";
 
-const HIDIVE_GROUP_ID = "5145258";
-const HIDIVE_PUBLICATIONS_COLLECTION_ID = "K2GPFB99";
-const BASE_URL = new URL(`https://api.zotero.org/groups/${HIDIVE_GROUP_ID}/`);
+let HIDIVE_GROUP_ID = "5145258";
+let HIDIVE_PUBLICATIONS_COLLECTION_ID = "K2GPFB99";
+let BASE_URL = new URL(`https://api.zotero.org/groups/${HIDIVE_GROUP_ID}/`);
 
-const authorSchema = z.object({
+let authorSchema = z.object({
   family: z.string(),
   given: z.string(),
 });
 
-const cslJsonSchema = z.object({
+let cslJsonSchema = z.object({
   id: z.string(),
   type: z.string(),
   title: z.string(),
@@ -33,18 +33,22 @@ const cslJsonSchema = z.object({
   })).optional(),
 });
 
-function parseBibEntry(xml: string): string {
-  const match = xml.match(/<div class="csl-right-inline"[^>]*>(.*?)<\/div>/);
-  assert(match, "Could not find the bib entry");
-  return he.decode(match[1].trim());
-}
-
-const zoteroItemResponse = z.object({
+let zoteroItemResponse = z.object({
   key: z.string(),
   version: z.number(),
   bib: z.string().transform(parseBibEntry),
   csljson: cslJsonSchema,
 });
+
+let ncbiIdConverterResponseSchema = z.object({
+  records: z.object({ doi: z.string(), pmid: z.string().optional() }).array(),
+});
+
+function parseBibEntry(xml: string): string {
+  let match = xml.match(/<div class="csl-right-inline"[^>]*>(.*?)<\/div>/);
+  assert(match, "Could not find the bib entry");
+  return he.decode(match[1].trim());
+}
 
 /**
  * Fetches the publications from the HiDive Zotero group.
@@ -52,43 +56,62 @@ const zoteroItemResponse = z.object({
  * @returns A list of publications.
  */
 async function fetchHidivePublications(): Promise<Array<string>> {
-  const url = new URL(
+  let url = new URL(
     `collections/${HIDIVE_PUBLICATIONS_COLLECTION_ID}/items`,
     BASE_URL,
   );
   url.searchParams.set("format", "keys");
   url.searchParams.set("itemType", "-attachment");
-  const response = await fetch(url);
-  const text = await response.text();
+  let response = await fetch(url);
+  let text = await response.text();
   return text.trim().split("\n");
 }
 
 type Item = z.infer<typeof zoteroItemResponse>;
 
+async function getPubMedIds(
+  dois: Array<string>,
+  batchSize = 100,
+): Promise<Record<string, string>> {
+  let records: Record<string, string> = {};
+  for (let i = 0; i < dois.length; i += batchSize) {
+    let batch = dois.slice(i, i + batchSize);
+    let url = new URL("https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/");
+    url.searchParams.set("ids", batch.join(","));
+    url.searchParams.set("format", "json");
+    let response = await fetch(url);
+    let data = await response.json();
+    for (let record of ncbiIdConverterResponseSchema.parse(data).records) {
+      if (record.pmid) records[record.doi] = record.pmid;
+    }
+  }
+  return records;
+}
+
 async function fetchZoteroItem(itemKey: string): Promise<Item> {
-  const url = new URL(`items/${itemKey}`, BASE_URL);
+  let url = new URL(`items/${itemKey}`, BASE_URL);
   url.searchParams.set("format", "json");
   url.searchParams.set("include", "csljson,bib");
   url.searchParams.set(
     "style",
     "https://gist.githubusercontent.com/manzt/743d185cc9e84fec0669aefb2d423d78/raw/d303835cf3bb82096e30a27be0f0225a42c65323/hidive.csl",
   );
-  const response = await fetch(url);
-  const data = await response.json();
-  const item = zoteroItemResponse.parse(data);
+  let response = await fetch(url);
+  let data = await response.json();
+  let item = zoteroItemResponse.parse(data);
   return item;
 }
 
 if (import.meta.main) {
-  const itemKeys = await fetchHidivePublications();
-  const pb = new ProgressBar({
+  let itemKeys = await fetchHidivePublications();
+  let pb = new ProgressBar({
     title: "Fetching publications",
     total: itemKeys.length,
   });
 
-  const items = [];
+  let items = [];
   let i = 0;
-  for (const itemKey of itemKeys) {
+  for (let itemKey of itemKeys) {
     try {
       items.push(await fetchZoteroItem(itemKey));
     } catch (error) {
@@ -97,19 +120,31 @@ if (import.meta.main) {
     await pb.render(i++);
   }
 
-  const final = items
+  let idMap = await getPubMedIds(
+    items
+      .map((item) => item.csljson.DOI)
+      .filter((d) => typeof d === "string"),
+  );
+
+  let final = items
+    // filter out items without authors
+    .filter((item) => item.csljson.author)
     // sort by date (newest first)
     .toSorted((a, b) => {
-      const dateA = new Date(
+      let dateA = new Date(
         a.csljson.issued?.year ?? 0,
         a.csljson.issued?.month ?? 0,
       );
-      const dateB = new Date(
+      let dateB = new Date(
         b.csljson.issued?.year ?? 0,
         b.csljson.issued?.month ?? 0,
       );
       return dateB.getTime() - dateA.getTime();
-    });
+    })
+    .map((item) => ({
+      pmid: item.csljson.DOI ? idMap[item.csljson.DOI] : undefined,
+      ...item,
+    }));
 
   Deno.writeTextFileSync("pubs.json", JSON.stringify(final, null, 2));
 }
---
+++ render.ts
@@ -0,0 +1,78 @@
+import { stringify } from "jsr:@std/csv";
+import * as colors from "jsr:@std/fmt@1.0.2/colors";
+let contents = Deno.readTextFileSync("pubs.json");
+let pubs = JSON.parse(contents);
+
+type RawAuthor = { given: string; family: string };
+type Author = { kind: "human"; given: string; family: string } | {
+  kind: "consortium";
+  name: string;
+};
+
+function formatGiven(given: string) {
+  let initials = given.match(/[A-Z]/g);
+  return initials?.join("") ?? "";
+}
+
+function formatAuthor(author: Author) {
+  if (author.kind === "consortium") {
+    return author.name;
+  }
+  let { given, family } = author;
+  return `${formatGiven(given)} ${family}`;
+}
+
+function formatAuthors(authors: Array<RawAuthor>) {
+  let fmt = processAuthors(authors).map(formatAuthor);
+  if (fmt.length === 2) return fmt.join(" and ");
+  return fmt.join(", ");
+}
+
+function isNetworkOrConsortium(author: RawAuthor) {
+  let test = author.family.toLowerCase();
+  return test.includes("network") || test.includes("consortium");
+}
+
+function processAuthors(authors: Array<RawAuthor>): Array<Author> {
+  return authors.map((author) => {
+    if (isNetworkOrConsortium(author)) {
+      return { kind: "consortium", name: author.family };
+    }
+    return { ...author, kind: "human" };
+  });
+}
+
+function formatCitation(
+  c: {
+    author: Array<{ given: string; family: string }>;
+    title: string;
+    "container-title": string;
+    issued: { year: number };
+    page: string;
+    volume: string;
+    issue: string;
+  },
+) {
+  let citation = `${formatAuthors(c.author)}, "${c.title}", `;
+  if (c["container-title"]) citation += `${c["container-title"]}, `;
+  if (c.volume) citation += `${c.volume}`;
+  if (c.issue) citation += `(${c.issue})`;
+  if (c.page) citation += `${(c.issue || c.volume) ? ":" : ""}${c.page}`;
+  if (c.issued) {
+    citation += `${
+      (c.issue || c.volume || c.page) ? " " : ""
+    }(${c.issued.year})`;
+  }
+  return citation + ".";
+}
+
+let rows = pubs.map(({ csljson: pub, pmid }) => ({
+  Month: pub.issued?.month,
+  Year: pub.issued?.year,
+  Citation: formatCitation(pub),
+  "PubMed ID": pmid,
+  DOI: pub.DOI,
+}));
+
+let csv = stringify(rows, { columns: Object.keys(rows[0]) });
+Deno.writeTextFileSync("pubs.csv", csv);
